41332d903574d028d6c5331d656a835870f3333b
Cuihtlauac ALVARADO <cuihtmlauac@tarides.com>
2024-06-21 13:18:27 +0200

    init proj

---
+++ cloud.ml
@@ -0,0 +1,3 @@
+let () =
+  Wmo.Stratus.nimbus |> print_endline;
+  Wmo.Cumulus.nimbus |> print_endline
---
+++ wmo.ml
@@ -0,0 +1,7 @@
+module Stratus = struct
+  let nimbus = "Nimbostratus (Ns)"
+end
+
+module Cumulus = struct
+  let nimbus = "Cumulonimbus (Cb)"
+end
